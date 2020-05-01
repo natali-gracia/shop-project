8c5df82cfb4ecfea2a28deb598043c3eed9ac757
natali-gracia <natali1gracia@gmail.com>
2020-05-01 16:58:19 +0300

    Created dropdown-cart with sms cart-empty in Header-top

--- src/App/Header/HeaderCart/HeaderCart.js
+++ src/App/Header/HeaderCart/HeaderCart.js
@@ -4,17 +4,25 @@ import './headercart.css'
 
 const HeaderCart = () => {
     return (
-        <div className='header-cart'>
-            <p>
-                <a href="/">
-                    <span className='shopping-bag-icon'>
-                    </span>
-                    <span className='product-quantity'>0</span>
-                    <span className='text'> item(s) - </span>
-                    <span className='price'>₴ 0</span>
-                </a>
-            </p>
-            
+        <div className="wrap">
+            <div className='header-cart'>                
+                <p className='top-cart'>
+                    <a href="/">
+                        <span className='shopping-bag-icon'>
+                        </span>
+                        <span className='product-quantity'>0</span>
+                        <span className='text'> item(s) - </span>
+                        <span className='price'>₴ 0</span>
+                    </a>
+                </p>
+                <div className="dropdown-cart">
+                    <div className="cart-empty">
+                        <p>You have no items in your shopping cart.</p>
+                    </div>
+                    <div className="cart-mini-header" style={{display: 'none'}}>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
