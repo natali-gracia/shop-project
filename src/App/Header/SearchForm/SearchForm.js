9a922415b043ecf1a28e84ed9aa830936527909b
natali-gracia <natali1gracia@gmail.com>
2020-05-04 12:01:56 +0300

    used hook useState in SearchForm

--- src/App/Header/SearchForm/SearchForm.js
+++ src/App/Header/SearchForm/SearchForm.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 import './searchform.css'
 
-class SearchForm extends Component {
+const SearchForm = () => {
 
-    state = {
-        value: ''
+    const [value, setValue] = useState('')
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        alert('Search query: ' + value);
+        setValue('')
     }
 
-    handleChange = (event) => {
-        this.setState({value: event.target.value})
-    }
-
-    render() {
-        return (
-            <div>
-                <form action='' method='' className='search-form'>
-                    <input type="text" className='search-input-field' placeholder='Search' value={this.state.value} onChange={this.handleChange} autoComplete="off"/>
-                    <span className='search-input-btn'>
-                        <input type="submit" value='Search' className='btn'/>
-                    </span>
-                </form>   
-            </div>
-        )
-    }
+    return (
+        <div>
+            <form action='' method='' className='search-form' onSubmit={handleSubmit}>
+                <input type="text" className='search-input-field' placeholder='Search' value={value} onChange={event => setValue(event.target.value)} autoComplete="off"/>
+                <span className='search-input-btn'>
+                    <input type="submit" value='Search' className='btn'/>
+                </span>
+            </form>   
+        </div>
+    )
 }
+
+
 
 export default SearchForm
