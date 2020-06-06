62f741172d71bc1c54871de4b774e10ebabf1209
natali-gracia <natali1gracia@gmail.com>
2020-06-06 13:19:05 +0300

    created ZoomImage component to ProductPage

--- src/App/Main/ProductPage/ProductPage.js
+++ src/App/Main/ProductPage/ProductPage.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './productpage.css'
 
+import ViewSlider from '../../../Components/ViewSlider'
+
+import ProductOptions from './../Products/ProductOptions'
+import Quantity from './../../../Components/quantity/Quantity'
+import ProductPrice from './../../../Components/productprice/ProductPrice'
+
 import productsData, {getProductsMap} from './../Products/productsData'
+import ZoomImage from './ZoomImage/ZoomImage'
 
 const productsArray = getProductsMap(productsData)
 
@@ -12,11 +19,68 @@ const ProductPage = ({
 
     const id = match.params.productId
 
-    console.log(productsArray[id].name)
+    const [quickViewImg, setQuickViewImg] = useState(productsArray[id].mainimage)
+    const [productCount, setProductCount] = useState(1)
+
+    const onIncrementClick = () => {
+        setProductCount(productCount + 1)
+    }
+    
+    const onDecrementClick = () => {
+        setProductCount(productCount - 1)
+    }
     
     return (
-        <div>
-            Hello!
+        <div className='product-page'>
+            <section className="wrap">
+                <div className="quickview-img col-md-4">
+                    <ZoomImage
+                        quickViewImg = {quickViewImg}
+                   />
+                    <div className="more-view">
+                        <ViewSlider 
+                            id={id}
+                            setQuickViewImg={setQuickViewImg}
+                        />
+                    </div>
+                </div>
+                <div className="quickview-shop col-md-7">
+                    <h2 className="narrow">{productsArray[id].name}</h2>
+                    <table className="quickview-base-info">
+                        <tbody>
+                            <tr><th>Availability</th><td>{productsArray[id].in_stock > 1 ? 
+                                productsArray[id].in_stock + ' in stock' 
+                                : <span className="product-one-left">Only 1 left!</span>
+                                }</td></tr>
+                            <tr><th>Product Type</th><td>{productsArray[id].type}</td></tr>
+                            <tr><th>Material</th><td>{productsArray[id].material}</td></tr>
+                        </tbody>
+                    </table>
+                    <p className='product-summary'>{productsArray[id].summary}</p>
+                    <ProductOptions/>
+                    <div className="product-price wrap">
+                        <span className='product-price-title'>Price:</span>
+                        <ProductPrice
+                            discount_price = {productsArray[id].discount_price}
+                            price = {productsArray[id].price}
+                            price_value={'price-value'}
+                        />
+                    </div>
+                    <div className="product-quantity wrap">
+                        <span>Quantity:</span>
+                        <Quantity
+                            productCount={productCount}
+                            onIncrementClick={onIncrementClick}
+                            onDecrementClick={onDecrementClick}
+                            minCount={1}
+                            maxCount={productsArray[id].in_stock}
+                        />                       
+                    </div>
+                    <div className="action-buttons">
+                        <button>Add to Cart</button>
+                    </div>
+                </div>               
+            </section>
         </div>
     )
 }
---
+++ src/App/Main/ProductPage/ZoomImage/ZoomImage.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+import './zoomimage.css'
+
+class ZoomImage extends Component {
+    constructor (props) {
+        super(props)
+
+        const {
+            quickViewImg
+        } = props
+
+        this.state = {
+            backgroundImage: `url(${quickViewImg})`,
+            backgroundPosition: '0% 0%',
+        }
+    }
+
+    handleMouseMove = e => {
+        const { left, top, width, height } = e.target.getBoundingClientRect()
+        const x = (e.pageX - left) / width * 100
+        const y = ((e.pageY - top) / height * 100) - 125
+        this.setState({ backgroundPosition: `${x}% ${y}%`})
+    }
+
+    handleMouseOver = (quickViewImg) => {
+        this.setState({
+            backgroundImage: `url(${quickViewImg})`
+        })
+    }
+
+    render () {
+        const {
+            quickViewImg
+        } = this.props;
+        return (
+            <figure  
+                style={this.state}
+                onMouseMove={this.handleMouseMove}
+                onMouseOver={()=>this.handleMouseOver(quickViewImg)} 
+                className='zoom-img-container'>
+                    <img src={quickViewImg} />
+            </figure>   
+)}}
+
+export default ZoomImage
